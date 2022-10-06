9da2a2ca83168b9143b8db778cd6a0ed94c4fbfc
Shifaan <53972679+Shifaan0010@users.noreply.github.com>
2022-10-06 16:14:02 +0530

    Connect to database; Disconnect on exit

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,30 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 import dotenv from 'dotenv';
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-const port = parseInt(process.env.PORT);
-console.log(`NODE_ENV = ${process.env.NODE_ENV} Port = ${port}`);
+// set env variables (PORT, MONGO_URI, ...) from file
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+console.log(
+    [
+        `NODE_ENV=${process.env.NODE_ENV}`,
+        `PORT=${process.env.PORT}`,
+        `MONGO_URI=${process.env.MONGO_URI}`,
+        `TOKEN_KEY=${process.env.TOKEN_KEY}`,
+    ].join('\n')
+);
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI);
+
+mongoose.connection.on('error', err => console.log(err));
+
+// if a path is specified in args, serve index.html from there
 if (process.argv[2]) {
     const indexPath = path.resolve(path.join(process.argv[2], 'index.html'));
     console.log(indexPath);
@@ -21,4 +33,19 @@ if (process.argv[2]) {
     app.get('/*', (req, res) => res.sendFile(indexPath));
 }
 
-app.listen(port, () => console.log(`Server listening at localhost:${port}`));
+const port = parseInt(process.env.PORT);
+const server = app.listen(port, () =>
+    console.log(`Server listening at localhost:${port}`)
+);
+
+// Close server and db on exit
+['SIGINT', 'SIGUSR1', 'SIGUSR2', 'SIGTERM'].forEach((signal) =>
+    process.on(signal, async () => {
+        console.log(`Recieved ${signal}`);
+        console.log('Closing express server');
+        server.close();
+        console.log('Closing mongodb client');
+        await mongoose.disconnect();
+        process.exit();
+    })
+);
