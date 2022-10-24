f89ed36e72dd68ab7006af7f821dcb63145fb499
Shifaan <53972679+Shifaan0010@users.noreply.github.com>
2022-10-24 13:15:42 +0530

    Store password hashes in db

--- server/src/database/auth.js
+++ server/src/database/auth.js
@@ -2,17 +2,28 @@ import mongoose from 'mongoose';
 
 import User from '../models/user.js';
 
+import crypto from 'crypto';
+
+const hashPassword = (password, salt) => {
+    const hash = crypto.createHash('sha256');
+    hash.update(password);
+    hash.update(salt);
+    return hash.digest('base64');
+}
+
 const verify_password = async (username, password) => {
     const user = await User.findOne({ username });
-    return user != null && user.passwordHash === password;
+    return user != null && user.passwordHash === hashPassword(password, user.passwordSalt);
 };
 
 const create_user = async (username, password) => {
     if ((await User.findOne({ username })) != null) {
         throw new Error('User already exists');
     } else {
-        const user = new User({ username, passwordHash: password });
+        const salt = crypto.randomBytes(512).toString();
+        const user = new User({ username, passwordSalt: salt, passwordHash: hashPassword(password, salt) });
         await user.save();
+        // console.log(user);
     }
 };
 
