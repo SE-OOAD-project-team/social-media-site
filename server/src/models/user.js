787a6b8dd6cb3a99f9971850fbd6d1b6c67a5529
Shifaan <53972679+Shifaan0010@users.noreply.github.com>
2022-10-24 18:14:39 +0530

    Update user.js

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 import { Schema } from 'mongoose';
 
-const UserSchema = new Schema({
-    username: String,
-    passwordHash: String,
-    passwordSalt: String,
-});
+const UserSchema = new Schema(
+    {
+        username: { type: String, required: true, index: { unique: true } },
+        passwordHash: { type: String, required: true },
+        passwordSalt: { type: String, required: true },
+    }
+);
 
 const User = mongoose.model('User', UserSchema);
 
