f25397c844165a54ef8f9b9feab1fb2b93ea4d55
Anish-Udupa <udupa.anish@gmail.com>
2022-11-14 12:50:40 +0530

    Upload Image boilerplate

--- client/src/components/create-post/CreatePost.js
+++ client/src/components/create-post/CreatePost.js
@@ -1,71 +1,86 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState, useRef } from "react";
 import "./CreatePost.css";
 
 function CreatePost(){
+  const fileRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [ hasUploadedImg, setHasUploadedImg ] = useState(false);
+  const [ imgObjectURL, setImgObjectURL ] = useState(null);
 
-  // drag state
-  const [dragActive, setDragActive] = useState(false);
-  // ref
-  const dragInputRef = useRef(null);
-  
-  // handle drag events
-  const handleDrag = function(e) {
-    e.preventDefault();
+  useEffect(() => {
+    if(hasUploadedImg) {
+      // Displaying the image on canvas and enabling cropping and editing the image.
+      // ERROR: Image is not being displayed!! Check here
+      const ctx = canvasRef.current.getContext("2d");
+      const img = new Image();
+      img.src = imgObjectURL
+      ctx.drawImage(img, 0, 0);
+    }
+  }, [hasUploadedImg])
+
+  const handleSelectedImage = (img) => {
+    // Things that need to be done on the image after uploading it
+    console.log(img.name);
+    if(imgObjectURL !== null)
+      URL.revokeObjectURL(imgObjectURL);
+    
+    const img_obj_url = window.URL.createObjectURL(img)
+    setImgObjectURL(img_obj_url);
+    
+    setHasUploadedImg(true);
+  }
+
+  const handleFileSelectorChange = () => {
+    // On clicking select file button (which is triggered by the handleFileBtnClick)
+    const file = fileRef.current.files[0];
+    handleSelectedImage(file); // Performs actions to the image
+    
+  }
+
+  const handleFileBtnClick = () => {
+    // Upload input trigger from clicking the dropbox
+    if(fileRef)
+      fileRef.current.click();
+  }
+
+  const stopDefault = (e) => {
+    // Preventing default behaviour of the events
     e.stopPropagation();
-    if (e.type === "dragenter" || e.type === "dragover") {
-      setDragActive(true);
-    } else if (e.type === "dragleave") {
-      setDragActive(false);
-    }
-  };
-  
-  // triggers when file is dropped
-  const handleDrop = function(e) {
     e.preventDefault();
-    e.stopPropagation();
-    setDragActive(false);
-    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
-      // handleFiles(e.dataTransfer.files);
-    }
-  };
-  
-  // triggers when file is selected with click
-  const handleChange = function(e) {
-    e.preventDefault();
-    if (e.target.files && e.target.files[0]) {
-      // handleFiles(e.target.files);
-    }
-  };
-  
-// triggers the input when the button is clicked
-  const onButtonClick = () => {
-    dragInputRef.current.click();
-  };
+  }
 
-    return (
-      <div id="create-post-container">
-        <p className="heading">Create a post</p>
-        <textarea className="create-post-input" placeholder="What do you wanna talk about?" maxLength={1000}></textarea>
-        <input className="create-post-input" type="text" placeholder="Hashtag" />
-        {/* Allow users to upload images here */}
+  const handleFileDrop = (e) => {
+    const dt = e.dataTransfer;
+    const file = dt.files[0];
+    handleSelectedImage(file); // Performs actions to the image
+    stopDefault(e);
+  }
 
-        <input ref={dragInputRef} type="images" id="input-file-upload" multiple={false} onChange={handleChange} />
-      <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
-        {/* <div> */}
-          <p>Drag and drop your file here or</p>
-          <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
-        {/* </div>  */}
-      </label>
-      { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
+  return (
+    <div id="create-post-container">
+      <p className="heading">Create a post</p>
+      <textarea className="create-post-input" placeholder="Say something about it..." maxLength={100}></textarea>
+      {/* Allow users to upload images here */}
+      <input type="file" id="img-upload" name="img-upload" accept="image/png, image/jpeg" ref={fileRef} onChange={handleFileSelectorChange} />
       
-        {/* Post visible to -> everyone, followers, private */}
-        <div id="post-btn-container">
-            <div className="btn post-ptn">POST</div>
-            <div className="btn discard-btn">DISCARD</div>
-        </div>
-      </div>  
-    );
+      {(hasUploadedImg === false) && (<div className="cp-drop-zone" onClick={handleFileBtnClick} onDragEnter={stopDefault} onDragOver={stopDefault} onDrop={handleFileDrop}>
+        <p>Drag and drop your image here</p>
+        <p>OR</p>
+        <p>Click here to upload an image</p>
+      </div>)}
+
+      {(hasUploadedImg) && (
+        // <img src={imgObjectURL} />  // This works!!
+        <canvas id="cp-canvas" ref={canvasRef}></canvas>
+      )}
+
+      <div id="post-btn-container">
+        <div className="cp-btn">POST</div>
+        <div className="cp-btn">DISCARD</div>
+      </div>
+    </div>  
+  );
 }
 
 export default CreatePost;
