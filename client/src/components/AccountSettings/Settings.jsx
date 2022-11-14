ea70aa83ddb4d6c8e02ae5c18d76e7363118e64f
Shifaan <53972679+Shifaan0010@users.noreply.github.com>
2022-11-14 19:44:30 +0530

    Added followers settings

--- client/src/components/AccountSettings/Settings.jsx
+++ client/src/components/AccountSettings/Settings.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Sidebar from './Sidebar';
 import ProfileSettings from './Profile';
 import PasswordSettings from './Password.jsx';
+import FollowingSettings from './Following.jsx';
 
 import homeImage from '../../assets/home.svg';
 
@@ -13,18 +14,23 @@ import { useNavigate, Link } from 'react-router-dom';
 
 const Settings = () => {
     const navigate = useNavigate();
-    
+
     const username = window.localStorage.getItem('username');
-    if (username == null) {
-        return navigate('/login');
-    }
 
-    const [selected, setSelected] = useState(0);
+    useEffect(() => {
+        if (username == null) {
+            navigate('/login');
+        }
+    });
 
-    const tabs = ['Profile', 'Password'];
-    const tab_components = [ProfileSettings, PasswordSettings].map(
-        (component) => React.createElement(component, { username })
-    );
+    const [selected, setSelected] = useState(1);
+
+    const tabs = ['Profile', 'Following', 'Password'];
+    const tab_components = [
+        ProfileSettings,
+        FollowingSettings,
+        PasswordSettings,
+    ].map((component) => React.createElement(component, { username }));
 
     return (
         <div className={`${style.Container}`}>
